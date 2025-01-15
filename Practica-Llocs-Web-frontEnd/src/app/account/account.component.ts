7d8028e681549c5a02531923a9e57a290644bb1b
Paul841 <153355150+Paul841@users.noreply.github.com>
2025-01-15 09:46:31 +0100

    Commit

--- Practica-Llocs-Web-frontEnd/src/app/account/account.component.ts
+++ Practica-Llocs-Web-frontEnd/src/app/account/account.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from "../login/login.component";
 import { SignupComponent } from "../signup/signup.component";
 import { LogoutComponent } from "../logout/logout.component";
 import { CurrentuserComponent } from "../currentuser/currentuser.component";
+import { RegistreService } from '../services/registre.service';
 
 @Component({
   selector: 'app-account',
@@ -14,6 +15,48 @@ import { CurrentuserComponent } from "../currentuser/currentuser.component";
 })
 export class AccountComponent {
   showSignup: boolean = false;
+
+
+    constructor() {}
+    sessionId: string = '';
+    userId: string | null = null;
+    llocEvent: string = 'compte';
+    tipusEvent: string = 'visita';
+
+    private RegistreService = inject(RegistreService);
+
+    ngOnInit() {
+      this.sessionId = localStorage.getItem('sessionId') || this.generateSessionId();
+      this.logVisit();
+    }
+
+    private generateSessionId(): string {
+      const sessionId = Math.random().toString(36).substr(2, 9);
+      localStorage.setItem('sessionId', sessionId);
+      return sessionId;
+    }
+
+    private logVisit(): void {
+      const registreData = {
+        sessionId: this.sessionId,
+        userId: localStorage.getItem('userId'), // Puede ser null si no hay un usuario logueado
+        llocEvent: this.llocEvent,
+        tipusEvent: this.tipusEvent,
+      };
+
+
+      console.log(`Page visited at: ${new Date().toISOString()}, Session ID: ${this.sessionId}`);
+      this.RegistreService.createRegistre(registreData ).subscribe(
+        (response: any) => {
+          console.log('Visit logged successfully:', response);
+        },
+        (error) => {
+          console.error('Error logging visit:', error);
+        }
+      );
+
+    }
+
 
   toggleSignup(event?: Event) {
     if (event) {
